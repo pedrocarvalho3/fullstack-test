9cb975274a8e3d64f5a6bc9ea596c42d45153743
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-23 14:51:37 -0300

    feat: :sparkles: implementando scroll para cima sempre que mudar de página

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import { CharacterDetails } from "./pages/CharacterDetails";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/character/:id" element={<CharacterDetails />} />

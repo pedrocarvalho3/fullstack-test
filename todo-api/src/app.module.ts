ed0ed6ef407c5a8e6e7e37a47b88ec37fcf16d30
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-24 12:12:35 -0300

    feat: :sparkles: setup do prisma service

--- todo-api/src/app.module.ts
+++ todo-api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [TodosModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}

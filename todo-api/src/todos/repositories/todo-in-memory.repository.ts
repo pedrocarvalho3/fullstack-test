a53b3e31af88698a4675e6770363c2ad8623b655
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-08-24 12:27:46 -0300

    feat: :sparkles: implementando repositorio in memory

---
+++ todo-api/src/todos/repositories/todo-in-memory.repository.ts
@@ -0,0 +1,63 @@
+// src/todos/repositories/todo-in-memory.repository.ts
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { TodoRepository } from './todos.repository';
+import { CreateTodoDto } from '../dto/create-todo.dto';
+import { UpdateTodoDto } from '../dto/update-todo.dto';
+import { Todo } from '../entities/todo.entity';
+
+@Injectable()
+export class TodoInMemoryRepository implements TodoRepository {
+  private todos: Todo[] = [];
+
+  async findAll(): Promise<Todo[]> {
+    return this.todos;
+  }
+
+  async findById(id: string): Promise<Todo | null> {
+    const todo = this.todos.find((todo) => todo.id === id);
+    return todo || null;
+  }
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const newTodo = new Todo({
+      id: uuidv4(),
+      title: createTodoDto.title,
+      description: createTodoDto.description || '',
+      completed: createTodoDto.completed || false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo | null> {
+    const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+
+    if (todoIndex === -1) {
+      return null;
+    }
+
+    const updatedTodo = new Todo({
+      ...this.todos[todoIndex],
+      ...updateTodoDto,
+      updatedAt: new Date(),
+    });
+
+    this.todos[todoIndex] = updatedTodo;
+    return updatedTodo;
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+
+    if (todoIndex === -1) {
+      return false;
+    }
+
+    this.todos.splice(todoIndex, 1);
+    return true;
+  }
+}
